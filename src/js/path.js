d9c5193b5af03c21da40ad90bda3b6ef62caa010
Félix Pedrozo <pedrozofelix6991@gmail.com>
2022-04-13 23:00:16 -0400

    feat: Se añaden nuevas funciones a la clase Path
    - Se crearon las funciones para obtener la ruta de los directorios del destination y model
    - Se modifica la funcion list mejorando la logica para obtener la ruta de los directorios de destination y model

--- src/js/path.js
+++ src/js/path.js
@@ -1,20 +1,49 @@
 class Path {
-    static fileContentPath = "src/file_content";
-
-    constructor(model, destination) {
+    constructor(model, destination, fileNameDestination) {
         this.model = model;
         this.destination = destination;
+        this.fileNameDestination = fileNameDestination;
     }
 
-    static list(nameFile) {
+    static directoryDestination() {
+        const directoryDAO = process.cwd();
+        //Se elimina de la ruta la carpeta /beans
+        return directoryDAO.substring(0, directoryDAO.length - 3);
+    }
+
+    static directoryModel() {
+        const directoryJs = __dirname;
+        //Se elimina de la ruta la carpeta /js
+        const directorySrc = directoryJs.substring(0, directoryJs.length - 3);
+        return `${directorySrc}/assets/baseFile`;
+    }
+
+    get fileDestination() {
+        return `${this.destination}${this.fileNameDestination}`;
+    }
+
+    static list(entityName) {
+        const directoryDestination = Path.directoryDestination();
+        const directoryModel = Path.directoryModel();
+
         return [
-            new Path(`${Path.fileContentPath}/Controller.java`, `${Path.fileContentPath}/test/controller/${nameFile}API.java`),
-            new Path(`${Path.fileContentPath}/ServiceImpl.java`, `${Path.fileContentPath}/test/service/${nameFile}ServiceImpl.java`)
+            new Path(`${directoryModel}/Controller.java`, `${directoryDestination}/controllers/`, `${entityName}API.java`),
+            new Path(`${directoryModel}/ServiceImpl.java`, `${directoryDestination}/services/`, `${entityName}ServiceImpl.java`),
+            new Path(`${directoryModel}/DAOImpl.java`, `${directoryDestination}/dao/`, `${entityName}DAOImpl.java`),
+            new Path(`${directoryModel}/TBL.java`, `${directoryDestination}/dao/tables/`, `${entityName}TBL.java`),
+
+            //Beans
+            new Path(`${directoryModel}/ResultWithPaginationDTO.java`, `${directoryDestination}/beans/`, `${entityName}ResultWithPagination.java`),
+            new Path(`${directoryModel}/ResultDTO.java`, `${directoryDestination}/beans/`, `${entityName}Result.java`),
+
+            //Interface
+            new Path(`${directoryModel}/DAOInterface.java`, `${directoryDestination}/dao/`, `${entityName}DAO.java`),
+            new Path(`${directoryModel}/ServiceInterface.java`, `${directoryDestination}/services/`, `${entityName}Service.java`),
         ];
     }
 
     toString() {
-        return `Model: ${this.model}, Destination: ${this.destination}`;
+        return `Model: ${this.model}, Destination: ${this.fileDestination}`;
     }
 }
 
