a37860980eaad2edae68786c5684d957e94580a9
Félix Pedrozo <pedrozofelix6991@gmail.com>
2022-04-13 23:12:46 -0400

    refactor: Modifique la funcion main extrayecndo su logica en otra funcion

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,7 @@
-const fs = require("fs");
-const Path = require("./path");
+const { generateCRUD } = require("./generate");
 
 const main = () => {
-    try {
-        const pathList = Path.list("Loan");
-
-        for (const path of pathList) {
-            console.log(path.toString());
-
-            const content = fs.readFileSync(path.model, "utf8");
-            const formatter = content.replaceAll("NotificationCenter", "Loan");
-
-            /* if (!fs.existsSync(path.destination)) {
-                fs.mkdirSync(path.destination, {recursive:true});
-            }
-        
-            fs.writeFileSync(path.destination, formatter); */
-        }
-    } catch(error) {
-        console.error("Problems generating the files. ", error);
-    }
+    generateCRUD("Loan");
 }
 
 main();
